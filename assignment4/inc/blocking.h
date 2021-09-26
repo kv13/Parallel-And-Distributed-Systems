f7bce9c7bd354755465f55ebb6273c0785416c43
kv13 <kostas33vergo@gmail.com>
2021-09-26 23:20:28 +0300

    Header files

---
+++ assignment4/inc/blocking.h
@@ -0,0 +1,47 @@
+#ifndef BLOCKING_H
+#define BLOCKING_H
+
+
+// ******* struct definitions *******
+typedef struct{
+    uint32_t *blk_nz;
+    uint32_t *blk_ids;
+    uint32_t *csc_col;
+    uint32_t *csc_row;
+    uint32_t nz, nzb;
+    uint32_t N, M, n_b, b;
+} blocked_csc;
+
+typedef struct{
+    uint32_t *blk_nz;
+    uint32_t *blk_ids;
+    uint32_t *csr_col;
+    uint32_t *csr_row;
+    uint32_t nz, nzb;
+    uint32_t N, M, n_b, b;
+} blocked_csr;
+
+typedef struct{
+    int blk_id;
+    int nzs;
+}block;
+
+// ******* functions declaration *******
+void blocked_csr_init(blocked_csr *blk_A, csr_format *csr_A, int b);
+void blocked_csc_init(blocked_csc *blk_B, csc_format *csc_B, int b);
+
+void blocking_csr3(blocked_csr *blk_A, csr_format *csr_A);
+void blocked_2_csr(blocked_csr *blk_A, csr_format *csr_AA);
+
+void blocking_csc3(blocked_csc *blk_B, csc_format *csc_B);
+void blocked_2_csc(blocked_csc *blk_B, csc_format *csc_BB);
+
+void insert_block(int *nzb, block *nzb_array, int blk_id);
+int  search_block(int *exist_flag, int blk_id, int l, int r, block *nnzb_array);
+
+int  find_row(int blk_id, int n_b, int b);
+int  find_col(int blk_id, int n_b, int b);
+int  find_relevant_row(int current_row,int blk_row_start);
+int  find_relevant_col(int current_col, int blk_col_start);
+
+#endif
---
+++ assignment4/inc/utils.h
@@ -0,0 +1,47 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+// ******* struct definitions *******
+
+// struct to read coo matrices from maret matrix
+typedef struct 
+{
+    int *coo_I, *coo_J;
+    int N;  // number of rows
+    int M;  // number of columns
+    int nz; // number of non zero elements
+
+} market_matrix;
+
+typedef struct 
+{
+    uint32_t *csr_col;
+    uint32_t *csr_row;
+    uint32_t N; // number of rows
+    uint32_t M; // number of columns
+    uint32_t nz;
+} csr_format;
+
+typedef struct 
+{
+    uint32_t *csc_col;
+    uint32_t *csc_row;
+    uint32_t N; // number of rows
+    uint32_t M; // number of columns
+    uint32_t nz;
+} csc_format;
+
+// ******* functions declaration *******
+int   binary_search(uint32_t *arr , int l, int r, int s_obj);
+int  binary_search2(uint32_t *arr1, uint32_t *arr2, int l, int r, int s_obj1, int s_obj2);
+int  partition(int *arr_1, int *arr_2, int l, int h);
+void read_mm_matrices(market_matrix *mtx, char *name);
+void coo2csc(uint32_t *const row, uint32_t *const col, uint32_t const *const row_coo, uint32_t const *const col_coo, uint32_t const nnz, uint32_t const n, uint32_t const isOneBased);
+void csr_init(csr_format *mtx1, market_matrix *mtx2);
+void csc_init(csc_format *mtx1, market_matrix *mtx2);
+void visualization_csc(uint32_t *csc_row,uint32_t *csc_col, int N, int M);
+void visualization_csr(uint32_t *csr_row,uint32_t *csr_col, int N, int M);
+void quick_sort(int *arr_1, int *arr_2, int l, int h);
+void swap(int *a, int *b);
+
+#endif
