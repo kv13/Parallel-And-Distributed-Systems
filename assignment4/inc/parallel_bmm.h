1d059876b352e520551aa6ba33cf03470a0f3db7
kv13 <kostas33vergo@gmail.com>
2021-09-29 19:08:06 +0300

    Shared Memory header files

---
+++ assignment4/inc/parallel_bmm.h
@@ -0,0 +1,13 @@
+#ifndef PARALLEL_BMM_H
+#define PARALLEL_BMM_H
+
+
+// ******* functions declaration ******* 
+
+// ******* blocked bmm functions
+void blocked_bmm_shared(coo_format *C, blocked_csr *blk_A, csr_format *AA, blocked_csc *blk_B, csc_format *BB);
+
+// ******* filtered blocked bmm functions
+void blocked_bmm_filtered_shared(coo_format *C, blocked_csr *blk_F,csr_format *FF,blocked_csr *blk_A, csr_format *AA, blocked_csc *blk_B, csc_format *BB);
+
+#endif
